da822f87cf58590b887a92528e2b91fef81247de
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-24 01:02:49 -0300

    create api services

--- src/context.jsx
+++ src/context.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-
+import axios from 'axios';
 const Context = createContext({});
 
 const ContextProvider = ({ children }) => {
@@ -14,7 +14,7 @@ const ContextProvider = ({ children }) => {
     console.log('click me');
   };
 
-  console.log(searchInputValue);
+  useEffect(() => {});
 
   return (
     <Context.Provider
